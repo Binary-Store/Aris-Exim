c300ca3fa8c2bb6cb6553adaa39db6581396baaa
utsav <utsavbusa222@gmail.com>
2025-02-10 16:12:12 +0530

    some change are done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
-(function($) {
+(function ($) {
     'use strict';
 
     /*-------------------------------------------------------------------------------
   Preloader
-	-------------------------------------------------------------------------------*/
-    $(window).on('load', function() {
+    -------------------------------------------------------------------------------*/
+    $(window).on('load', function () {
         $('.ct-preloader').addClass('hidden');
     });
 
     /*-------------------------------------------------------------------------------
   Aside Menu
-	-------------------------------------------------------------------------------*/
+    -------------------------------------------------------------------------------*/
 
-    $(".aside-trigger-right").on('click', function() {
+    $(".aside-trigger-right").on('click', function () {
         var $el = $(".sigma_aside-right-panel");
         $el.toggleClass('open');
         if ($el.hasClass('open')) {
-            setTimeout(function() {
+            setTimeout(function () {
                 $el.find('.sidebar').fadeIn();
             }, 300);
         } else {
@@ -24,34 +24,21 @@
         }
     });
 
-    $(".aside-trigger-left").on('click', function() {
+    $(".aside-trigger-left").on('click', function () {
         $(".sigma_aside-left").toggleClass('open');
     });
 
-    $(".sigma_aside .menu-item-has-children > a").on('click', function(e) {
+    $(".sigma_aside .menu-item-has-children > a").on('click', function (e) {
         var submenu = $(this).next(".sub-menu");
         e.preventDefault();
 
         submenu.slideToggle(200);
     });
 
-    /*-------------------------------------------------------------------------------
-    Cart Trigger
-    -------------------------------------------------------------------------------*/
-    $(".cart-trigger i").on('click', function(e) {
-        $(this).parent().toggleClass('open');
-    });
-
-    /*-------------------------------------------------------------------------------
-    Search Trigger
-    -------------------------------------------------------------------------------*/
-    $(".search-trigger").on('click', function(e) {
-        $(".search-form-wrapper").toggleClass('open');
-    });
 
     /*-------------------------------------------------------------------------------
   Sticky Header
-	-------------------------------------------------------------------------------*/
+    -------------------------------------------------------------------------------*/
     var header = $(".can-sticky");
     var headerHeight = header.innerHeight();
 
@@ -66,7 +53,7 @@
 
     /*-------------------------------------------------------------------------------
   Aside Scroll
-	-------------------------------------------------------------------------------*/
+    -------------------------------------------------------------------------------*/
     function initAsideScrollbar() {
         var scrollHeight = $('.main-aside').innerHeight() - $(".main-aside .navbar-brand").innerHeight(); // Calculate the height of the scroll container
         var calculatedHeight = isNaN(scrollHeight) ? "auto" : scrollHeight;
@@ -85,12 +72,12 @@
     /*-------------------------------------------------------------------------------
     Count Up
     -------------------------------------------------------------------------------*/
-    $('.counter-number span').each(function() {
+    $('.counter-number span').each(function () {
         $(this).animate({
             Counter: $(this).text()
         }, {
             duration: 2000,
-            step: function(now) {
+            step: function (now) {
                 $(this).text(Math.ceil(now));
             }
         });
@@ -99,10 +86,10 @@
     /*-------------------------------------------------------------------------------
     Progress bar on view
     -------------------------------------------------------------------------------*/
-    $(".sigma_progress").each(function() {
+    $(".sigma_progress").each(function () {
         var progressBar = $(this).find(".progress-bar");
         var progressCount = $(this).find(".sigma_progress-count");
-        $(progressBar).on('inview', function(event, isInView) {
+        $(progressBar).on('inview', function (event, isInView) {
             if (isInView) {
                 $(progressBar).animate({
                     width: $(progressBar).attr("aria-valuenow") + "%"
@@ -172,7 +159,7 @@
             settings: {
                 slidesToShow: 1,
             }
-        }, ]
+        },]
     });
 
     /*-------------------------------------------------------------------------------
@@ -187,19 +174,19 @@
         prevArrow: $('.banner .slider-prev'),
         nextArrow: $('.banner .slider-next'),
         responsive: [{
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                    dots: true
-                }
-            },
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+                dots: true
+            }
+        },
         ]
     });
 
@@ -213,25 +200,25 @@
         arrows: true,
         dots: true,
         responsive: [{
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                }
-            },
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+            }
+        },
         ]
     });
 
     /*-------------------------------------------------------------------------------
     Masonry
     -------------------------------------------------------------------------------*/
-    $('.masonry').imagesLoaded(function() {
+    $('.masonry').imagesLoaded(function () {
         var isotopeContainer = $('.masonry');
         isotopeContainer.isotope({
             itemSelector: '.masonry-item',
@@ -241,13 +228,13 @@
     /*-------------------------------------------------------------------------------
     Product List/Grid Toggle
     -------------------------------------------------------------------------------*/
-    $(".toggle-grid").on('click', function() {
+    $(".toggle-grid").on('click', function () {
         $(".view-toggler i").removeClass('active');
         $(this).addClass('active');
         $('.ct-product').removeClass('product-list');
         $('.ct-product').parent().removeClass('col-md-12').addClass('col-md-6');
     })
-    $(".toggle-list").on('click', function() {
+    $(".toggle-list").on('click', function () {
         $(".view-toggler i").removeClass('active');
         $(this).addClass('active');
         $('.ct-product').addClass('product-list');
@@ -255,14 +242,14 @@
     })
 
     //On scroll events
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
 
         doSticky();
 
     });
 
     //On resize events
-    $(window).on('resize', function() {
+    $(window).on('resize', function () {
 
         initAsideScrollbar();
 
